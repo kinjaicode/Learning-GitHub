4e5734bdc99a1b589f3aaff14666be2bf3126952
WongTzeKin <tzekinwong@icloud.com>
2022-12-30 21:03:15 +0800

    Initial Commit

---
+++ LearningGitHub/LearningGitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGitHubApp.swift
+//  LearningGitHub
+//
+//  Created by 黃子健 on 30/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct LearningGitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
